6c16ce7551b96ab2e6c564d5e7aa2782150c0859
Jihyeon <jh418531@gmail.com>
2026-02-06 16:06:38 +0900

    feat: 404 오류 페이지 추가

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
     <div className="flex flex-1 min-h-auto flex-col ">
       <div className="flex flex-1 flex-col justify-center items-center my-auto">
         <Image src="/assets/images/not-found.png" alt="not found icon" width="100" height="100" />
-        <h1 className="text-2xl font-bold text-center mt-8">포스트를 찾을 수 없습니다</h1>
+        <h1 className="text-2xl font-bold text-center mt-8">페이지를 찾을 수 없습니다</h1>
         <Link href="/">
           <Button className="font-bold mt-6 cursor-pointer" size="lg">
             홈으로 돌아가기
